ead0a97b66b620cd9ead6c8de987c55c5700bddc
Rick Wolchuk <rickwolchuk@gmail.com>
2014-09-11 12:40:54 -0500

    Initial Commit

---
+++ AFNetworkingTNG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFNetworkingTNG
+//
+//  Created by Rick Wolchuk on 9/11/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFNetworkingTNG/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AFNetworkingTNG
+//
+//  Created by Rick Wolchuk on 9/11/14.
+//  Copyright (c) 2014 Rick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
